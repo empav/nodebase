4e52a363dcb016e20d5d46e43e7b4530ef6ed867
Emanuele Pavan <emanuele.pav@gmail.com>
2025-11-03 09:18:39 +0100

    changes theme && style improvements

--- src/features/auth/components/SignForm.tsx
+++ src/features/auth/components/SignForm.tsx
@@ -24,6 +24,7 @@ import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import Image from "next/image";
 
 const loginSchema = z.object({
   email: z.email("Please enter a valid email address"),
@@ -102,68 +103,65 @@ const SignForm = ({ isLogin }: SignFormProps) => {
   const isSubmitting = form.formState.isSubmitting;
 
   return (
-    <div className="flex h-full items-center justify-center">
-      <Card className="min-w-[500px]">
-        <CardHeader className="text-center">
-          <CardTitle>Welcome back</CardTitle>
-          <CardDescription>
-            {isLogin ? "Login to continue" : "Register a new account"}
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+    <Card className="min-w-[500px]">
+      <CardHeader className="text-center">
+        <CardTitle className="flex items-center justify-center mb-2 gap-x-2">
+          <Image alt="Logo" src="/logos/logo.svg" width={60} height={60} />{" "}
+          Nodebase
+        </CardTitle>
+        <CardTitle>Welcome back</CardTitle>
+        <CardDescription>
+          {isLogin ? "Login to continue" : "Register a new account"}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="grid gap-6">
+              <div className="flex flex-col gap-4">
+                <Button
+                  variant={"outline"}
+                  className="w-full"
+                  type="button"
+                  disabled={isSubmitting}
+                  onClick={onSocialSign("github")}
+                >
+                  <Image
+                    alt="Github logo"
+                    src="/logos/github.svg"
+                    width={20}
+                    height={20}
+                  />
+                  Continue with Github
+                </Button>
+                <Button
+                  variant={"outline"}
+                  className="w-full"
+                  type="button"
+                  disabled={isSubmitting}
+                  onClick={onSocialSign("google")}
+                >
+                  <Image
+                    alt="Google logo"
+                    src="/logos/google.svg"
+                    width={20}
+                    height={20}
+                  />
+                  Continue with Google
+                </Button>
+              </div>
               <div className="grid gap-6">
-                <div className="flex flex-col gap-4">
-                  <Button
-                    variant={"outline"}
-                    className="w-full"
-                    type="button"
-                    disabled={isSubmitting}
-                    onClick={onSocialSign("github")}
-                  >
-                    Continue with Github
-                  </Button>
-                  <Button
-                    variant={"outline"}
-                    className="w-full"
-                    type="button"
-                    disabled={isSubmitting}
-                    onClick={onSocialSign("google")}
-                  >
-                    Continue with Google
-                  </Button>
-                </div>
-                <div className="grid gap-6">
-                  {isLogin ? null : (
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Name</FormLabel>
-                          <FormControl>
-                            <Input
-                              type="string"
-                              placeholder="Full name (first and last name)"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  )}
+                {isLogin ? null : (
                   <FormField
                     control={form.control}
-                    name="email"
+                    name="name"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Email</FormLabel>
+                        <FormLabel>Name</FormLabel>
                         <FormControl>
                           <Input
-                            type="email"
-                            placeholder="m@example.com"
+                            type="string"
+                            placeholder="Full name (first and last name)"
                             {...field}
                           />
                         </FormControl>
@@ -171,12 +169,48 @@ const SignForm = ({ isLogin }: SignFormProps) => {
                       </FormItem>
                     )}
                   />
+                )}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="m@example.com"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="********"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {isLogin ? null : (
                   <FormField
                     control={form.control}
-                    name="password"
+                    name="confirmPassword"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Password</FormLabel>
+                        <FormLabel>Confirm Password</FormLabel>
                         <FormControl>
                           <Input
                             type="password"
@@ -188,62 +222,43 @@ const SignForm = ({ isLogin }: SignFormProps) => {
                       </FormItem>
                     )}
                   />
-                  {isLogin ? null : (
-                    <FormField
-                      control={form.control}
-                      name="confirmPassword"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Confirm Password</FormLabel>
-                          <FormControl>
-                            <Input
-                              type="password"
-                              placeholder="********"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  )}
-                  <Button
-                    type="submit"
-                    className="w-full"
-                    disabled={isSubmitting}
-                  >
-                    {isLogin ? "Login" : "Sign up"}
-                  </Button>
-                </div>
-                <div className="text-center text-sm">
-                  {isLogin ? (
-                    <>
-                      Don&apos;t have an account?{" "}
-                      <Link
-                        href={"/signup"}
-                        className="underline underline-offset-4"
-                      >
-                        Sign up
-                      </Link>
-                    </>
-                  ) : (
-                    <>
-                      <Link
-                        href={"/login"}
-                        className="underline underline-offset-4"
-                      >
-                        Login
-                      </Link>{" "}
-                      with your account
-                    </>
-                  )}
-                </div>
+                )}
+                <Button
+                  type="submit"
+                  className="w-full"
+                  disabled={isSubmitting}
+                >
+                  {isLogin ? "Login" : "Sign up"}
+                </Button>
               </div>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
+              <div className="text-center text-sm">
+                {isLogin ? (
+                  <>
+                    Don&apos;t have an account?{" "}
+                    <Link
+                      href={"/signup"}
+                      className="underline underline-offset-4"
+                    >
+                      Sign up
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link
+                      href={"/login"}
+                      className="underline underline-offset-4"
+                    >
+                      Login
+                    </Link>{" "}
+                    with your account
+                  </>
+                )}
+              </div>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
